77a2309648527168a1333dca5a06382fc2fe3f82
Nuwanssu <nuwanssu@cf.lk>
2025-08-12 10:25:30 +0530

    [1.0.0] - Initial commit

---
+++ src/main/java/lk/StudyReview/study_review/StudyReviewApplication.java
@@ -0,0 +1,13 @@
+package lk.StudyReview.study_review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyReviewApplication.class, args);
+	}
+
+}
