6b2d6fcc2ffa4ef8019a34dc211a33701e2eaa23
Nuwanssu <nuwanssu@cf.lk>
2025-10-31 12:26:08 +0530

    [1.0.0] - Submit assignment 2

--- src/main/java/lk/StudyReview/study_review/controller/TopicController.java
+++ src/main/java/lk/StudyReview/study_review/controller/TopicController.java
@@ -13,7 +13,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/topic")
